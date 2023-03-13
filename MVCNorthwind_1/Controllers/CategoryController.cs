bca4dc91198daff0a888e8fbfd34dff0f732ea7e
cagri.yolyapar <cagri.yolyapar@gmail.com>
2023-03-13 14:28:15 +0300

    Add project files.

---
+++ MVCNorthwind_1/Controllers/CategoryController.cs
@@ -0,0 +1,52 @@
+﻿using MVCNorthwind_1.DesignPatterns.SingletonPattern;
+using MVCNorthwind_1.Models;
+using MVCNorthwind_1.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCNorthwind_1.Controllers
+{
+    public class CategoryController : Controller
+    {
+        NorthwindEntities _db;
+
+        public CategoryController()
+        {
+            _db = DBTool.DbInstance;
+        }
+        public ActionResult ListCategories()
+        {
+            List<CategoryVM> categories = _db.Categories.Select(x => new CategoryVM
+            {
+                ID = x.CategoryID,
+                CategoryName = x.CategoryName,
+                Description = x.Description,
+            }).ToList();
+            
+            return View(categories);
+        }
+
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCategory(CategoryVM cvm)
+        {
+            Category c = new Category
+            {
+                CategoryName = cvm.CategoryName,
+                Description = cvm.Description
+            };
+            _db.Categories.Add(c);
+            _db.SaveChanges();
+
+            return RedirectToAction("ListCategories");
+        }
+            
+    }
+}
---
+++ MVCNorthwind_1/ViewModels/CategoryVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCNorthwind_1.ViewModels
+{
+    public class CategoryVM
+    {
+        public int ID { get; set; }
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+       // public List<string> ProductNames { get; set; }
+    }
+}
